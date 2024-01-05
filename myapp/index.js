959c7ffebf844057b7ef2113d7593f535463f939
Charan Kumar <charanprabhas29.com>
2024-01-05 20:11:25 +0000

    server update

--- myapp/index.js
+++ myapp/index.js
@@ -1,21 +1,9 @@
 const express = require("express");
+
+const path = require("path");
+
 const app = express();
-const sqlite3 = require("sqlite3");
 
-// Connecting Database
-let db = new sqlite3.Database(":memory:", (err) => {
-  if (err) {
-    console.log("Error Occurred - " + err.message);
-  } else {
-    console.log("DataBase Connected");
-  }
-});
-
-app.get("/", (req, res) => {
-  res.send("GeeksforGeeks");
-});
-
-// Server Running
-app.listen(4000, () => {
-  console.log("Server started");
+app.listen(3000, () => {
+  console.log("Server running");
 });
