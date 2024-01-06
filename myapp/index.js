1392785dedffaeb25d6381888c1034d84c0fa2cd
Charan Kumar <charanprabhas29.com>
2024-01-06 12:10:13 +0000

    Finishing code

--- myapp/index.js
+++ myapp/index.js
@@ -3,12 +3,17 @@ const path = require("path");
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const app = express();
 app.use(express.json());
 
 const dbPath = path.join(__dirname, "notesread.db");
 
 let db = null;
+
+// initialization of DatabaseAndServer
 
 const initializeDatabaseAndServer = async () => {
   try {
@@ -27,9 +32,32 @@ const initializeDatabaseAndServer = async () => {
 
 initializeDatabaseAndServer();
 
+// Middleware Function for all the APIs
+
+const authenticationToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_KEY", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        next();
+      }
+    });
+  }
+};
+
 // get all notes API GET Method
 
-app.get("/notes", async (request, response) => {
+app.get("/notes", authenticationToken, async (request, response) => {
   const getNotesQuery = `
     SELECT
     *
@@ -40,9 +68,75 @@ app.get("/notes", async (request, response) => {
   response.send(notesArray);
 });
 
-app.post("/notes", async (request, response) => {
+// Register user API
+
+app.post("/register", async (request, response) => {
+  const { username, password, gender } = request.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const userQuery = `
+    SELECT 
+    *
+    FROM
+    users
+    WHERE username='${username}';`;
+  try {
+    const dbUser = await db.get(userQuery);
+    if (dbUser === undefined) {
+      const createUserQuery = `
+                INSERT INTO users (username, password, gender)
+                VALUES (
+                    '${username}', '${hashedPassword}', '${gender}'
+                );`;
+      await db.run(createUserQuery);
+      response.status(200);
+      response.send("User Created Successfully");
+    } else {
+      response.status(400);
+      response.send("User already exists");
+    }
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+});
+
+// Login user API
+
+app.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  const fetchUserQuery = `
+    SELECT 
+    *
+    FROM
+    users
+    WHERE username='${username}';`;
+
+  try {
+    const dbUser = await db.get(fetchUserQuery);
+
+    if (dbUser === undefined) {
+      response.status(200);
+      response.send("Invalid user");
+    } else {
+      const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+      if (isPasswordMatched === true) {
+        const payload = { username: username };
+        const jwtToken = jwt.sign(payload, "MY_SECRET_KEY");
+        response.send(jwtToken);
+      } else {
+        response.status(400);
+        response.send("Invalid Password");
+      }
+    }
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+});
+
+//Create notes API POST Method
+
+app.post("/notes", authenticationToken, async (request, response) => {
   const notesDetails = request.body;
-  const { notes_id, title, data, created_at } = notesDetails;
+  const { title, data, created_at } = notesDetails;
   const createNotesQuery = `
     INSERT INTO notesRead (title, data, created_at)
     VALUES (
@@ -59,7 +153,9 @@ app.post("/notes", async (request, response) => {
   }
 });
 
-app.get("/notes/:id", async (request, response) => {
+// get Notes of particular id API
+
+app.get("/notes/:id", authenticationToken, async (request, response) => {
   const notesId = request.params.id;
   const getNotesQuery = `
     SELECT
@@ -75,7 +171,31 @@ app.get("/notes/:id", async (request, response) => {
   }
 });
 
-app.delete("/notes/:id", async (request, response) => {
+// Update Notes API
+
+app.put("/notes/:id", authenticationToken, async (request, response) => {
+  const notesId = request.params.id;
+  console.log(notesId);
+  const updatedNotesDetails = request.body;
+  const { title, data, created_at } = updatedNotesDetails;
+  const updateNotesQuery = `
+    UPDATE notesRead
+    SET 
+        title='${title}',
+        data='${data}',
+        created_at='${created_at}'
+    WHERE notes_id = ${notesId};`;
+  try {
+    const dbResponse = await db.run(updateNotesQuery);
+    response.send("Notes Updated Successfully");
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+});
+
+// Delete notes API
+
+app.delete("/notes/:id", authenticationToken, async (request, response) => {
   const notesId = request.params.id;
   const getNotesQuery = `
     DELETE
